ed7e9d67e5b9169405825b4b31aecfdc564e28ee
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-26 20:05:20 +0530

    Added the error text alignment

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,8 +15,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
+import { Loader } from "lucide-react";
 
 const SignupForm = () => {
+  const isLoading = false;
+
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
@@ -36,71 +39,80 @@ const SignupForm = () => {
       <div className="sm:w-420 flex-center flex-col">
         <img src="public/assets/images/logo.svg" />
 
-        <h2 className="h2-bold md:h2-bold pt-2 sm:pt-1">
+        <h2 className="h2-bold md:h2-bold pt-1 sm:pt-1">
           Create a new account
         </h2>
         <p className="text-light-3 small-medium md:base-regular ">
-          To use Snapgram enter your account details
+          To use Snapgram, please enter your account details
         </p>
       </div>
 
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-1  mt-2"
+        className="flex flex-col gap-1 w-420 mt-2"
       >
         <FormField
           control={form.control}
           name="name"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="forms-field">
               <FormLabel>Name</FormLabel>
               <FormControl>
-                <Input type="text" className="shad-input" {...field} />
+                <Input type="text" className="shad-input max-h-10" {...field} />
               </FormControl>
-              <FormMessage />
+              <FormMessage className="error-message" />
+            </FormItem>
+          )}
+        />
+        <FormField  
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="forms-field">
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input max-h-10" {...field} />
+              </FormControl>
+              <FormMessage className="error-message" />
             </FormItem>
           )}
         />
         <FormField
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-                <FormField
-          control={form.control}
           name="email"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="forms-field">
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input type="text" className="shad-input" {...field} />
+                <Input type="text" className="shad-input max-h-10" {...field} />
               </FormControl>
-              <FormMessage />
+              <FormMessage className="error-message" />
             </FormItem>
           )}
         />
-                <FormField
+        <FormField
           control={form.control}
           name="password"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="forms-field">
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input type="password" className="shad-input" {...field} />
+                <Input type="password" className="shad-input max-h-10" {...field} />
               </FormControl>
-              <FormMessage />
+              <FormMessage className="error-message" />
             </FormItem>
           )}
         />
-        <Button type="submit" className="shad-button_primary mt-1">Submit</Button>
+        <Button type="submit" className="shad-button_primary mt-8">
+          {isLoading ? (
+            <div className="flex-center gap-2 ">
+              <Loader />
+              Loading...
+            </div>
+          ) : (
+            "Sign up "
+          )}
+        </Button>
       </form>
     </Form>
   );
